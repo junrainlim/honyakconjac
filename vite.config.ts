a075d07a4150e3f54918bdce00756dd629ef0ad8
Jun Lim <150909838+junrainlim@users.noreply.github.com>
2024-01-15 00:36:46 +0800

    Update vite.config.ts
    github page fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "https://junrainlim.github.io/honyakconjac/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
